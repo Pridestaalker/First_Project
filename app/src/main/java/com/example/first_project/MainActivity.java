3d5788cc9febcac0d2e17016bd2eaea19152d0d3
swuit <lehouelleur@et.esiea.fr>
2020-05-07 17:54:19 +0200

    Ajout API

--- app/src/main/java/com/example/first_project/MainActivity.java
+++ app/src/main/java/com/example/first_project/MainActivity.java
@@ -6,11 +6,24 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://pokeapi.co";
+
 
     private RecyclerView recyclerView;
     private ListAdapter mAdapter;
@@ -20,23 +33,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
 
             protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        showList();
+        makeApiCall();
 
-            recyclerView.setHasFixedSize(true);
-            // use a linear layout manager
-            layoutManager = new LinearLayoutManager(this);
-            recyclerView.setLayoutManager(layoutManager);
+    }
 
-            final List<String> input = new ArrayList<>();
-            for (int i = 1; i < 10; i++) {
-                input.add("Test" + i);
-            }
+    private void showList() {
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
-            mAdapter = new ListAdapter(input);
-            recyclerView.setAdapter(mAdapter);
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        final List<String> input = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            input.add("Test" + i);
+        }
+
+        mAdapter = new ListAdapter(input);
+        recyclerView.setAdapter(mAdapter);
 
         ItemTouchHelper.SimpleCallback simpleItemTouchCallback =
                 new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
@@ -45,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                             target) {
                         return false;
                     }
+
                     @Override
                     public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
                         input.remove(viewHolder.getAdapterPosition());
@@ -53,7 +73,49 @@ public class MainActivity extends AppCompatActivity {
                 };
         ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
         itemTouchHelper.attachToRecyclerView(recyclerView);
+    }
 
 
+    private void makeApiCall() {
+
+                Gson gson = new GsonBuilder()
+                        .setLenient()
+                        .create();
+
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(BASE_URL)
+                        .addConverterFactory(GsonConverterFactory.create(gson))
+                        .build();
+
+                PokeApi pokeApi = retrofit.create(PokeApi.class);
+
+                Call<RestPokemonResponse> call = pokeApi.getPokemonResponse();
+                call.enqueue(new Callback<RestPokemonResponse>() {
+                    @Override
+                    public void onResponse(Call<RestPokemonResponse> call, Response<RestPokemonResponse> response) {
+                        if(response.isSuccessful() && response.body() != null) {
+                            List<Pokemon> championList = response.body().getResults();
+                            Toast.makeText(getApplicationContext(), "API SUCCESS", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            showError();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<RestPokemonResponse> call, Throwable t) {
+                        showError();
+
+                    }
+
+
+                });
+
+
+    }
+
+    private void showError() {
+
+        Toast.makeText(getApplicationContext(), "API ERROR", Toast.LENGTH_SHORT).show();
     }
 }
