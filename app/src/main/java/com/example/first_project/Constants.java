518e58b4dee1ec07ec6d7c28198f8f77c25bed47
swuit <lehouelleur@et.esiea.fr>
2020-05-16 15:53:02 +0200

    ADD mvc architecture

--- app/src/main/java/com/example/first_project/Constants.java
+++ app/src/main/java/com/example/first_project/Constants.java
@@ -3,4 +3,7 @@ package com.example.first_project;
 public class Constants {
 
     public static String KEY_POKEMON_LIST = "jsonPokemonList";
+
+    public static final String BASE_URL = "https://pokeapi.co";
+
 }
--- app/src/main/java/com/example/first_project/presentation/view/ListAdapter.java
+++ app/src/main/java/com/example/first_project/presentation/view/ListAdapter.java
@@ -1,4 +1,4 @@
-package com.example.first_project;
+package com.example.first_project.presentation.view;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,6 +7,7 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.first_project.R;
 import com.example.first_project.presentation.model.Pokemon;
 
 import java.util.List;
